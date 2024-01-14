cdc0e5de48db9f8fdaa0e23b69ef45955f0dbf6c
Raju <csta.puja@gmmail.com>
2024-01-14 15:27:14 -0500

    navigation graph and viewbinding added

--- app/src/main/java/com/example/tdd/ui/AddShoppingItemFragment.kt
+++ app/src/main/java/com/example/tdd/ui/AddShoppingItemFragment.kt
@@ -1,17 +1,54 @@
 package com.example.tdd.ui
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.example.tdd.R
+import com.example.tdd.databinding.FragmentAddShoppingItemBinding
 
-class AddShoppingItemFragment : Fragment(R.layout.fragment_add_shopping_item) {
+class AddShoppingItemFragment : Fragment() {
 
+    private var _binding: FragmentAddShoppingItemBinding? = null
+    private val binding get() = _binding!!
 
     lateinit var viewModel: ShoppingViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentAddShoppingItemBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        viewModel = ViewModelProvider(requireActivity()).get(ShoppingViewModel::class.java)
+        viewModel = ViewModelProvider(requireActivity())[ShoppingViewModel::class.java]
+
+        binding.btnAddShoppingItem.setOnClickListener {
+            findNavController().navigate(
+                R.id.action_addShoppingItemFragment_to_imagePickFragment
+            )
+        }
+
+        val callback = object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                viewModel.setCurrentUrl("")
+                findNavController().popBackStack()
+            }
+        }
+        requireActivity().onBackPressedDispatcher.addCallback(callback)
     }
 }
--- app/src/main/java/com/example/tdd/ui/ImagePickFragment.kt
+++ app/src/main/java/com/example/tdd/ui/ImagePickFragment.kt
@@ -1,14 +1,35 @@
 package com.example.tdd.ui
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.tdd.R
+import com.example.tdd.databinding.FragmentImagePickBinding
 
-class ImagePickFragment : Fragment(R.layout.fragment_image_pick) {
+class ImagePickFragment : Fragment() {
 
-    lateinit var viewModel: ShoppingViewModel
+    private var _binding: FragmentImagePickBinding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var viewModel: ShoppingViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentImagePickBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel = ViewModelProvider(requireActivity()).get(ShoppingViewModel::class.java)
